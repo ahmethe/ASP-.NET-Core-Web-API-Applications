4d2fd0ca20225d9fd90219fae0c006125d110f08
ahmethe <ahmeterden1705@gmail.com>
2024-12-12 17:59:55 +0300

    Entity Framework Core has been added.

---
+++ WebApi/Controllers/BooksController.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Repositories;
+
+/* 
+<summary>
+    Burada constructor üzerinde görüldüğü gibi bir dependency injection patterni
+    mevcut. Burada IoC ile containerdan ilgili ifade resolve ediliyor framework tarafından.
+</summary>
+*/
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+        public BooksController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            try // veritabanı işlemleri risklidir. Bu işlemler gerçekleştirilirken oluşacak hataları yakalayabilmek için try catch kullanılır.
+            {
+                var books = _context.Books.ToList();
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var book = _context
+                    .Books
+                    .Where(b => b.Id.Equals(id))
+                    .SingleOrDefault();
+
+                if (book is null)
+                    return NotFound(); //404
+
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+            try
+            {
+                if (book is null)
+                    return BadRequest(); //400
+
+                _context.Books.Add(book);
+                _context.SaveChanges(); //Değişikliklerin veritabanına yansıması için yapılır.
+
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id,
+            [FromBody] Book book)
+        {
+            try
+            {
+                var entity = _context
+                    .Books
+                    .Where(b => b.Id.Equals(id))
+                    .SingleOrDefault();
+
+                if (entity is null)
+                    return NotFound();
+
+                if (id != book.Id)
+                    return BadRequest();
+
+                entity.Title = book.Title; //entity zaten takip edildiği için mappingden sonra SaveChanges yapmak yeterlidir.
+                entity.Price = book.Price;
+
+                _context.SaveChanges();
+
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var entity = _context
+                    .Books
+                    .Where(b => b.Id.Equals(id))
+                    .SingleOrDefault();
+
+                if (entity is null)
+                    return NotFound(new
+                    {
+                        statusCode = 404,
+                        message = $"Book with id:{id} could not found."
+                    });
+
+                _context.Books.Remove(entity);
+                _context.SaveChanges();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPatch("{id:int}")]
+        public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id,
+            [FromBody] JsonPatchDocument<Book> bookPatch)
+        {
+            try
+            {
+                var entity = _context
+                    .Books
+                    .Where(b => b.Id.Equals(id))
+                    .SingleOrDefault();
+
+                if (entity is null)
+                    return NotFound();
+
+                bookPatch.ApplyTo(entity);
+                _context.SaveChanges(); //değişikliğin işleyebilmesi için yapılmalı.
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ WebApi/Repositories/RepositoryContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+using WebApi.Repositories.Config;
+
+namespace WebApi.Repositories
+{
+    /* 
+    <summary>
+        DbContext kodda yazdığımız modelin veritabanına 
+        ilişkileriyle yansıtılmasını sağlayan metotları içeren bir sınıftır.
+        Modelimize ait sınıfa DbSet diyerek bu sınıfın veritabanında bir tablo
+        gibi davranmasını sağlarız.
+        Bu DbContexti kullanabilmek için bunu bir connectionString ile
+        ilişkilendirmemiz gerekir. Bunu da constructor aracılığıyla yaparız.
+        Connection String ifademizi appsettings.json dosyasına yazarız.
+        Bu uygulamada tek bir veritabanından gidileceği için direkt appsettings.json
+        dosyasına yazıldı. Fakat genel olarak development modda sqlLite product
+        modda sql server kullanılır.
+        Daha sonra servis kaydı için Program.cs dosyasına gidilir. AddDbContext ile
+        servis kaydı gerçekleştirilir. Bu bizim ihtiyacımız olduğunda bu contextin
+        concrete halini elde etmemizi sağlar. Burada IoC(Inversion of Control) mekanizması
+        çalışır. Biz bir framework kullanıyoruz. Yeri geldiğinde buna müdahale ederek
+        kontrolü tersine çevirmiş oluyoruz. IoC register, resolve ve dispose işlemlerinden
+        sorumludur. Program.cs'te (uygulamanın başlangıç noktası) yaptığımz şey register, ihtiyaç olduğunda bize concrete 
+        ifadeyi vermesi resolve, oluşturulan nesnenin yaşam süresine karar verme dispose işlemi
+        olarak adlandırılır.
+    </summary>
+    */
+    public class RepositoryContext : DbContext 
+    {
+        public RepositoryContext(DbContextOptions options) :
+            base(options)
+        {
+            
+        }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfig());
+        }
+    }
+}
