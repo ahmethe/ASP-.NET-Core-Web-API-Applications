b2ff5940d15f75923c5dda866d817c9b910a05ec
ahmethe <ahmeterden1705@gmail.com>
2024-12-16 16:53:42 +0300

    Global error handling.

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ba64548fa0580c4bb880dc643f8ef941251c615")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dc3211dce2e7a164c9a1e51998a2c61cadc2bab")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
